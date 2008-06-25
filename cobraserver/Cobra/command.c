566c369ecaf23849cd1da910950669ba0a3ce9e5
search.pythoner <search.pythoner@25290add-f64c-0410-b0c9-f33bc836ec67>
2008-06-25 09:04:00 +0000

    init import

---
+++ cobraserver/Cobra/command.c
@@ -0,0 +1,105 @@
+#include "Python.h"
+#include "frameobject.h"
+#include "code.h"
+#include "cobra.h"
+
+char buf[10240];
+
+
+
+void ClearBuffer() {
+	memset(buf, 0, 10240);
+}
+
+char* GetConsts(PyFrameObject* frame) {
+	PyCodeObject* code = frame->f_code;
+	PyObject* consts = code->co_consts;
+	char* tmp = NULL;
+
+	ClearBuffer();
+	tmp = PyString_AsString(PyObject_Str(consts));
+	memcpy(buf, tmp, 10240);
+	return buf;
+}
+
+char* GetNames(PyFrameObject* frame) {
+	PyCodeObject* code = frame->f_code;
+	PyObject* names = code->co_names;
+	char* tmp = NULL;
+
+	ClearBuffer();
+	tmp = PyString_AsString(PyObject_Str(names));
+	memcpy(buf, tmp, 10240);
+	return buf;
+}
+
+char* GetLocals(PyFrameObject* frame) {
+	char* tmp = NULL;
+
+	ClearBuffer();
+	tmp = PyString_AsString(PyObject_Str(frame->f_locals));
+	memcpy(buf, tmp, 10240);
+	return buf;
+}
+
+char* GetGlobals(PyFrameObject* frame) {
+	char* tmp = NULL;
+
+	ClearBuffer();
+	tmp = PyString_AsString(PyObject_Str(frame->f_globals));
+	memcpy(buf, tmp, 10240);
+	return buf;
+}
+
+char* GetStack(PyFrameObject* frame, PyObject** stack_pointer) {
+	PyCodeObject* co = frame->f_code;
+	char stackValue[1024];
+	int i = 0;
+	int count = 0;
+	char* value;
+
+#define STACK_LEVEL()	((int)(stack_pointer - frame->f_valuestack))
+
+	memset(stackValue, 0, 1024);
+	count = STACK_LEVEL();
+	for( ; i < count; ++i) {
+		PyObject* obj = stack_pointer[-(i+1)];
+		value = PyString_AsString(PyObject_Str(obj));
+		strcat(stackValue, value);
+		strcat(stackValue, "*");
+	}
+
+	ClearBuffer();
+	sprintf(buf, "%d\n%d\n%s", co->co_stacksize, STACK_LEVEL(), stackValue);
+	return buf;
+}
+
+char* GetByteCodePath(PyFrameObject* frame) {
+	//构造并初始化名字集合
+	char tmp[100];
+	char* names[100];
+	int index = 0;
+	int i = 0;
+	int instruction_offset = frame->f_lasti;
+	memset(tmp, 0, 100*sizeof(char));
+	memset(names, 0, 100*sizeof(char*));
+
+	while(frame != NULL) {
+		PyCodeObject* co = frame->f_code;
+		char* name = PyString_AsString(co->co_name);
+		if(strcmp(name, "<module>") == 0) {
+			name = "$";
+		}
+		names[index++] = name;
+		frame = frame->f_back;
+	}
+
+	ClearBuffer();
+	for( ; i < index; ++i) {
+		strcat(buf, names[i]);
+		strcat(buf, ".");
+	}
+	sprintf(tmp, "%d", instruction_offset);
+	strcat(buf, tmp);
+	return buf;
+}
---
+++ cobraserver/Include/cobra.h
@@ -0,0 +1,45 @@
+#ifndef Cobra_HEADER_H
+#define Cobra_HEADER_H
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Python.h"
+#include "frameobject.h"
+
+#ifndef _WINSOCK2API_
+#include <winsock2.h>
+#endif
+
+void CleanupWinsock(void);
+int InitWinsock(void);
+
+void PythonServerListen(int port);
+void AcceptPythonClient();
+void SendPythonClient();
+
+void VmServerListen(int port);
+void AcceptVmClient();
+void SendVmClient(char* data);
+
+char* ReceivePythonCommand();
+char* ReceiveVmCommand();
+void ClosePythonClient();
+void CloseVmClient();
+
+void SetScriptName(char* name);
+char* GetScriptName();
+
+//÷¥––√¸¡Ó
+char* GetConsts(PyFrameObject* frame);
+char* GetNames(PyFrameObject* frame);
+char* GetStack(PyFrameObject* frame, PyObject** stack_pointer);
+char* GetByteCodePath(PyFrameObject* frame);
+char* GetLocals(PyFrameObject* frame);
+char* GetGlobals(PyFrameObject* frame);
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif /* !Cobra_HEADER_H*/
